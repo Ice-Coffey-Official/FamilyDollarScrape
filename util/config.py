d43f9fe99f35d6c435d9090aeef9ce1f43b71118
Ice-Coffey-Official <167157587+Ice-Coffey-Official@users.noreply.github.com>
2024-05-24 17:22:35 -0700

    hotfix for timeout issues

--- util/config.py
+++ util/config.py
@@ -1,3 +1,5 @@
 baseURL = 'https://www.familydollar.com/locations/'
 saveName = 'data'
 saveAs = ['csv', 'excel']
+reqSleep = 0 #how many seconds between each request
+retryBackoff = 4 #how many seconds between failed requests
--- util/links.py
+++ util/links.py
@@ -1,10 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+from util.config import retryBackoff, reqSleep
 
 def extractLinks(url):
+    time.sleep(reqSleep)
     newLinks = []
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    page = requests.get(url, headers=headers)
+    try:
+        page = requests.get(url, headers=headers)
+    except:
+        try:
+            time.sleep(retryBackoff)
+            page = requests.get(url, headers=headers)
+        except:
+            return []
     soup = BeautifulSoup(page.text, 'lxml')
     mylinks = soup.findAll("a", { "class" : "ga_w2gi_lp" })
     for i in range(len(mylinks)):
@@ -12,22 +22,43 @@ def extractLinks(url):
         if(len(link['href'])<=len(url) or link['href'] in newLinks):
             continue
         newLinks.append(link['href'])
-        if(i>2):
-            break
 
     return newLinks
 
 def extractStoreInfo(url):
+    time.sleep(reqSleep)
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    page = requests.get(url, headers=headers)
+    try:
+        page = requests.get(url, headers=headers)
+    except:
+        try:
+            time.sleep(retryBackoff)
+            page = requests.get(url, headers=headers)
+        except:
+            return ['Error', 'Error', 'Error', 'Error', 'Error', 'Error', 'Error', 'Error', 'Error']
     soup = BeautifulSoup(page.text, 'lxml')
     storeNum = url.split('/')[-2]
     city = url.split('/')[-3]
     state = url.split('/')[-4]
     name = soup.find("title").text.strip()
-    phoneNumber = soup.findAll("div", { "class" : "phone desktopPhone" })[-1].text
-    address = soup.find("span", { "itemprop" : "address" }).text.strip()
-    latitude = soup.find("meta", { "property" : "place:location:latitude" })['content']
-    longitude = soup.find("meta", { "property" : "place:location:longitude" })['content']
+    try:
+        phoneNumber = soup.findAll("div", { "class" : "phone desktopPhone" })[-1].text
+    except:
+        phoneNumber = ""
+
+    try:
+        address = soup.find("span", { "itemprop" : "address" }).text.strip()
+    except:
+        address = ""
+
+    try:
+        latitude = soup.find("meta", { "property" : "place:location:latitude" })['content']
+    except:
+        latitude = ""
+
+    try:
+        longitude = soup.find("meta", { "property" : "place:location:longitude" })['content']
+    except:
+        longitude = ""
     
     return [name, storeNum, phoneNumber, address, url, longitude, latitude, city, state]
